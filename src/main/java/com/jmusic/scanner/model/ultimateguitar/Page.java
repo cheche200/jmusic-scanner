4dc3314a948b2491cd2452825a5fb94d94670668
Jose Calderon <josecalderon@Joses-Air.lan>
2021-11-29 20:46:43 +0000

    Done dome cleanup on the model

--- src/main/java/com/jmusic/scanner/model/ultimateguitar/Page.java
+++ src/main/java/com/jmusic/scanner/model/ultimateguitar/Page.java
@@ -1,12 +1,9 @@
 
 package com.jmusic.scanner.model.ultimateguitar;
 
-import javax.annotation.Generated;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Page {
